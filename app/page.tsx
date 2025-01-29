1564406f6dc1f4e8bce220726fceee832dddfcc4
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-01-28 22:52:25 -0800

    ADD footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "../app/components/footer";
+
 const colors = [
   { name: "Primary", value: "bg-primary text-black" },
   { name: "Secondary", value: "bg-secondary text-black" },
@@ -10,19 +12,22 @@ const colors = [
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-white font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full max-w-md">
-          {colors.map((color) => (
-            <div
-              key={color.name}
-              className={`p-4 rounded-sm ${color.value} text-center font-semibold`}
-            >
-              {color.name}
-            </div>
-          ))}
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow grid items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 bg-white font-[family-name:var(--font-geist-sans)]">
+        <div className="flex flex-col gap-8 items-center sm:items-start">
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full max-w-md">
+            {colors.map((color) => (
+              <div
+                key={color.name}
+                className={`p-4 rounded-sm ${color.value} text-center font-semibold`}
+              >
+                {color.name}
+              </div>
+            ))}
+          </div>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
