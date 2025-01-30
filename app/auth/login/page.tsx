f9efd9cdb0cb021c71a48a1b84ae788b82fa47cd
Jason Chow <jchow149@my.bcit.ca>
2025-01-30 09:35:18 -0800

    [NEST-FE-10] Add signup page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -32,8 +32,8 @@ export default function LoginPage() {
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+              Login
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -102,7 +102,7 @@ export default function LoginPage() {
                 Sign in
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
+                Don’t have an account yet?
                 <a
                   href="#"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
