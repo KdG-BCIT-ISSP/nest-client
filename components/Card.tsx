5467ba496c8b28835440f40e74f81640751a887c
Juntingg <jsu62@my.bcit.ca>
2025-01-30 00:12:35 -0800

    add Card Component

---
+++ components/Card.tsx
@@ -0,0 +1,17 @@
+export default function Card() {
+  return (
+    <a
+      href="#"
+      className="block m-4 max-w-sm p-6 bg-container rounded-lg shadow-sm hover:bg-muted dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        What Nobody Told Me About Postpartum Recovery
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Before having my baby, I spent so much time preparing for the birth that
+        I didn’t fully understand what recovery afterward would be like.
+        Postpartum recovery is a ...
+      </p>
+    </a>
+  );
+}
