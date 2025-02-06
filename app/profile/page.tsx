0211c759aa87c60542c673da91f1a73f2bb07327
Juntingg <jsu62@my.bcit.ca>
2025-02-05 20:57:42 -0800

    [NEST-FE-23-User-profile-page] add field validation and format code

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -20,34 +20,34 @@ export default function ProfilePage() {
     <div className="p-4 sm:ml-64">
       <SideMenu></SideMenu>
       <div className="container mr-auto max-w-5xl">
-      <div className="flex items-center gap-3 py-2 pl-10">
-        {/* Avatar Section */}
-        <div className="flex flex-col items-center">
-          <Image
-            src="/images/default_profile_image.png"
-            className="object-cover rounded-md shrink-0 md:w-16 md:h-16 dark:border-none"
-            alt="avatar"
-            width={70}
-            height={70}
-            priority
-          />
-        </div>
+        <div className="flex items-center gap-3 py-2 pl-10">
+          {/* Avatar Section */}
+          <div className="flex flex-col items-center">
+            <Image
+              src="/images/default_profile_image.png"
+              className="object-cover rounded-md shrink-0 md:w-16 md:h-16 dark:border-none"
+              alt="avatar"
+              width={70}
+              height={70}
+              priority
+            />
+          </div>
 
-        {/* Text Info Section */}
-        <div className="flex flex-col justify-center ml-3">
-          <h5 className="text-md">userName</h5>
-          <p className="text-gray-500 text-sm">23456@gmail.com</p>
-          <button className="mt-2 border-secondary border-2 rounded-md text-sm text-secondary hover:text-white hover:bg-secondary">
-            Upload Image
-          </button>
+          {/* Text Info Section */}
+          <div className="flex flex-col justify-center ml-3">
+            <h5 className="text-md">userName</h5>
+            <p className="text-gray-500 text-sm">23456@gmail.com</p>
+            <button className="mt-2 border-secondary border-2 rounded-md text-sm text-secondary hover:text-white hover:bg-secondary">
+              Upload Image
+            </button>
+          </div>
         </div>
+        <ProfileInputField
+          username="Alice"
+          email="123@gmail.com"
+          region="north-america"
+        ></ProfileInputField>
       </div>
-      <ProfileInputField
-        username="Alice"
-        email="123@gmail.com"
-        region="north-america"
-      ></ProfileInputField>
-    </div>
     </div>
   );
 }
--- components/ProfileInputField.tsx
+++ components/ProfileInputField.tsx
@@ -2,143 +2,140 @@ import { ProfileDataType } from "@/types/ProfileDataType";
 import { useState } from "react";
 
 const REGION_VALUES = [
-    { value: "", label: "Select a region" },
-    { value: "north-america", label: "North America" },
-    { value: "europe", label: "Europe" },
-    { value: "south-america", label: "South America" },
+  { value: "", label: "Select a region" },
+  { value: "north-america", label: "North America" },
+  { value: "europe", label: "Europe" },
+  { value: "south-america", label: "South America" },
 ];
 
 export default function ProfileInputField({
-    username,
-    email,
-    region,
+  username,
+  email,
+  region,
 }: ProfileDataType) {
-    const [inputUsername, setInputUsername] = useState(username);
-    const [inputRegion, setInputRegion] = useState(region);
+  const [inputUsername, setInputUsername] = useState(username);
+  const [inputRegion, setInputRegion] = useState(region);
 
-    const [errors, setErrors] = useState({
-        username: "",
-        region: "",
-    });
+  const [errors, setErrors] = useState({
+    username: "",
+    region: "",
+  });
 
-    const validateForm = () => {
-        let isValid = true;
-        const newErrors = { username: "", region: "" };
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = { username: "", region: "" };
 
-        if (!inputUsername) {
-            newErrors.username = "Username can not be empty";
-            isValid = false;
-        }
-        if (inputUsername.length > 30) {
-            newErrors.username = "Characters can not exceed 30";
-            isValid = false;
-        }
-        
+    if (!inputUsername) {
+      newErrors.username = "Username can not be empty";
+      isValid = false;
+    }
+    if (inputUsername.length > 30) {
+      newErrors.username = "Characters can not exceed 30";
+      isValid = false;
+    }
 
-        if (!inputRegion) {
-            newErrors.region = "Please select a region";
-            isValid = false;
-        }
+    if (!inputRegion) {
+      newErrors.region = "Please select a region";
+      isValid = false;
+    }
 
-        setErrors(newErrors);
-        return isValid;
-    };
+    setErrors(newErrors);
+    return isValid;
+  };
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
 
-        if (validateForm()) {
-            console.log("Form is valid!");
-        } else {
-            console.log("Form is invalid.");
-        }
-    };
+    if (validateForm()) {
+      console.log("Form is valid!");
+    } else {
+      console.log("Form is invalid.");
+    }
+  };
 
-    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputUsername(event.target.value);
-    };
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputUsername(event.target.value);
+  };
 
-    const handleRegionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setInputRegion(event.target.value);
-    };
+  const handleRegionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setInputRegion(event.target.value);
+  };
 
-    return (
-        <div className="bg-white border border-2 rounded-md relative m-10">
-            <div className="p-6 space-y-6">
-                <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-3">
-                            <label
-                                htmlFor="product-name"
-                                className="text-sm font-medium text-gray-900 block mb-2"
-                            >
-                                Username
-                            </label>
-                            <input
-                                type="text"
-                                name="product-name"
-                                id="product-name"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                                value={inputUsername}
-                                onChange={handleUsernameChange}
-                                
-                            />
-                            {errors.username && (
-                                <p className="text-red-500 text-sm">{errors.username}</p>
-                            )}
-                        </div>
-                        <div className="col-span-6 sm:col-span-3">
-                            <label
-                                htmlFor="category"
-                                className="text-sm font-medium text-gray-900 block mb-2"
-                            >
-                                Email
-                            </label>
-                            <input
-                                type="text"
-                                name="category"
-                                id="category"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                                value={email}
-                                readOnly
-                            />
-                        </div>
-                        <div className="col-span-6 sm:col-span-3">
-                            <label
-                                htmlFor="brand"
-                                className="text-sm font-medium text-gray-900 block mb-2"
-                            >
-                                Region
-                            </label>
-                            <select
-                                name="region"
-                                id="region"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                                value={inputRegion}
-                                onChange={handleRegionChange}
-                            >
-                                {REGION_VALUES.map((region) => (
-                                    <option key={region.value} value={region.value}>
-                                        {region.label}
-                                    </option>
-                                ))}
-                            </select>
-                            {errors.region && (
-                                <p className="text-red-500 text-sm">{errors.region}</p>
-                            )}
-                        </div>
-                    </div>
-                    <div className="p-6 mt-6 border-t border-gray-200 flex justify-end">
-                        <button
-                            className="text-white bg-secondary hover:bg-tertiary font-medium rounded-md text-sm px-5 py-2.5 text-center"
-                            type="submit"
-                        >
-                            Save
-                        </button>
-                    </div>
-                </form>
+  return (
+    <div className="bg-white border border-2 rounded-md relative m-10">
+      <div className="p-6 space-y-6">
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="product-name"
+                className="text-sm font-medium text-gray-900 block mb-2"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                name="product-name"
+                id="product-name"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                value={inputUsername}
+                onChange={handleUsernameChange}
+              />
+              {errors.username && (
+                <p className="text-red-500 text-sm">{errors.username}</p>
+              )}
             </div>
-
-        </div>
-    );
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="category"
+                className="text-sm font-medium text-gray-900 block mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                name="category"
+                id="category"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                value={email}
+                readOnly
+              />
+            </div>
+            <div className="col-span-6 sm:col-span-3">
+              <label
+                htmlFor="brand"
+                className="text-sm font-medium text-gray-900 block mb-2"
+              >
+                Region
+              </label>
+              <select
+                name="region"
+                id="region"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                value={inputRegion}
+                onChange={handleRegionChange}
+              >
+                {REGION_VALUES.map((region) => (
+                  <option key={region.value} value={region.value}>
+                    {region.label}
+                  </option>
+                ))}
+              </select>
+              {errors.region && (
+                <p className="text-red-500 text-sm">{errors.region}</p>
+              )}
+            </div>
+          </div>
+          <div className="p-6 mt-6 border-t border-gray-200 flex justify-end">
+            <button
+              className="text-white bg-secondary hover:bg-tertiary font-medium rounded-md text-sm px-5 py-2.5 text-center"
+              type="submit"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
