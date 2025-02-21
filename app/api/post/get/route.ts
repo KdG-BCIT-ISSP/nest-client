0951f69d58b103328830b41a6c5c8a526053086c
Juntingg <jsu62@my.bcit.ca>
2025-02-21 01:53:47 -0800

    [NEST-FE-26]add getPost route

---
+++ app/api/post/get/route.ts
@@ -0,0 +1,9 @@
+import axiosInterceptor, { CustomAxiosRequestConfig } from "../../axiosInterceptor";
+
+
+export const getPost = async () => {
+    const response = await axiosInterceptor.get("/article", {
+        requiresAuth: true,
+    } as CustomAxiosRequestConfig);
+    return response.data;
+};
