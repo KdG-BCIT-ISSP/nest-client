81f6ddf052c089d8d74094a7860c1b7b7f55356b
Juntingg <jsu62@my.bcit.ca>
2025-03-05 18:48:08 -0800

    [NEST-FE-31]connect to update article api

---
+++ app/api/article/update/route.ts
@@ -0,0 +1,30 @@
+import axiosInterceptor, {
+    CustomAxiosRequestConfig,
+} from "../../axiosInterceptor";
+
+export const updateArticle = async (
+    id: number,
+    title: string,
+    content: string,
+    topicId: number,
+    type: string,
+    tags: string[],
+    images: string
+) => {
+    const requestData = { id, title, content, topicId, type, tags, images };
+    console.log("Request Data:", requestData);
+    const response = await axiosInterceptor.put(
+        "/article",
+        {
+            id: id,
+            title: title,
+            content: content,
+            topicId: topicId,
+            type: type,
+            tagNames: tags,
+            coverImage: images,
+        },
+        { requiresAuth: true } as CustomAxiosRequestConfig
+    );
+    return response.data;
+};
