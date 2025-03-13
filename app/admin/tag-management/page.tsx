ec579a635ca3623507b1a5176c11a004f3f68965
Juntingg <jsu62@my.bcit.ca>
2025-03-13 00:04:35 -0700

    [NEST-FE-51]create tag-management page

---
+++ app/admin/tag-management/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import SideMenu from "@/components/SideMenu";
+import UserRoleToggle from "@/components/UserRoleToggle";
+import { useEffect, useState } from "react";
+
+export default function TagManagementPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="p-4 sm:ml-64 bg-white">
+          <SideMenu />
+          <div className="container mr-auto max-w-5xl">
+{/* search bar */}
+        <form className="max-w-md mx-auto">
+          <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              </svg>
+            </div>
+            <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter email..." required />
+            <button type="submit" className="text-secondary absolute end-2.5 bottom-2.5 hover:text-tertiary font-medium rounded-md text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700">Search</button>
+          </div>
+        </form>
+      
+
+
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Username
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Role
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+
+            
+              <tr className="bg-white dark:bg-gray-800">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">
+                  Black
+                </td>
+                <td className="px-6 py-4">
+                  <UserRoleToggle />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+            
+          </div>
+        </div>
+  );
+}
