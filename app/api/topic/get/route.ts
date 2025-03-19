982e55b9dbae9ba576bb21543fda7ae588ca5c17
Juntingg <jsu62@my.bcit.ca>
2025-03-18 23:48:18 -0700

    [NEST-FE-55]Create Topic create, update, get, delete routes

---
+++ app/api/topic/get/route.ts
@@ -0,0 +1,10 @@
+import axiosInterceptor, {
+    CustomAxiosRequestConfig,
+} from "../../axiosInterceptor";
+
+export const getTopic = async () => {
+    const response = await axiosInterceptor.get("/topic", {
+        requiresAuth: true,
+    } as CustomAxiosRequestConfig);
+    return response.data;
+};
