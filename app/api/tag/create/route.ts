80fbbe734fff78de909988fc6d8e58bbe7363c35
Juntingg <jsu62@my.bcit.ca>
2025-03-18 23:47:57 -0700

    [NEST-FE-55]Create Tag create, update, get, delete routes

---
+++ app/api/tag/create/route.ts
@@ -0,0 +1,16 @@
+import axiosInterceptor, {
+    CustomAxiosRequestConfig,
+} from "../../axiosInterceptor";
+
+export const createTag = async (
+    name: string,
+) => {
+    const response = await axiosInterceptor.post(
+        "/tag/create",
+        {
+            name: name,
+        },
+        { requiresAuth: true } as CustomAxiosRequestConfig
+    );
+    return response.data;
+};
