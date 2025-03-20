95006a6a84347f803e6714ffd2a3de50c9176028
Juntingg <jsu62@my.bcit.ca>
2025-03-19 20:48:49 -0700

    [NEST-FE-55]add admin on dropdown menu in navbar for testing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,6 +35,7 @@ export default function Navbar() {
   ];
 
   const USER_DROPDOWN_LINKS = [
+    { href: "/admin/user-access", label: t("navigation.admin") },
     { href: "/profile", label: t("navigation.profile") },
     { href: "/profile/saved-posts", label: t("navigation.savedPosts") },
     { href: "/profile/notifications", label: t("navigation.notifications") },
