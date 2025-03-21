acddb78b1ba89eeac645cbd15e4caefb61ff1ea4
Juntingg <jsu62@my.bcit.ca>
2025-03-21 12:21:47 -0700

    [NEST-FE-52]add forgot-password page and adjust login, signup page

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -61,14 +61,15 @@ export default function LoginPage() {
 
   return (
     <section className="pt-5">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-        
+      <div className="flex flex-col items-center justify-center px-6 mx-auto lg:py-0">
 
-        <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 mt-10">
+
+        <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 mt-20">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
-              Login
+              Reset Password
             </h1>
+            <p className="text-md text-gray-800 dark:text-gray-400">You will receive a link to reset your password.</p>
 
             {error && (
               <p className="text-red-500 text-sm text-center">{error}</p>
@@ -96,66 +97,26 @@ export default function LoginPage() {
                   required
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      htmlFor="remember"
-                      className="text-gray-500 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-                <Link
-                  href="/auth/forgot-password"
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </Link>
-              </div>
+
+
 
               <button
                 type="submit"
-                className="w-full text-white bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 disabled:opacity-50"
+                
+                className="w-full text-white bg-secondary text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 disabled:opacity-50"
                 disabled={isLoading}
               >
-                {isLoading ? "Logging in..." : "Sign in"}
+                {isLoading ? "Logging in..." : "Send"}
               </button>
 
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?
+                Go Back to
                 <Link
-                  href="/auth/signup"
-                  className="ml-2 font-medium by-secondary hover:underline dark:text-primary-500"
+                  href="/auth/login"
+                  className="ml-2 font-medium hover:underline dark:text-primary-500"
                 >
-                  Sign up
+                  Sign in
                 </Link>
               </p>
             </form>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -77,19 +77,7 @@ export default function SignupPage() {
   return (
     <section className="pt-5">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-        <Link
-          href="/"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          <Image
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-            alt="logo"
-            width={32}
-            height={32}
-          />
-          NEST
-        </Link>
+        
 
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -167,7 +155,7 @@ export default function SignupPage() {
 
               <button
                 type="submit"
-                className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 disabled:opacity-50"
+                className="w-full text-white bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 disabled:opacity-50"
                 disabled={isLoading}
               >
                 {isLoading ? "Signing up..." : "Sign Up"}
@@ -177,7 +165,7 @@ export default function SignupPage() {
                 Already have an account?
                 <Link
                   href="/auth/login"
-                  className="ml-2 font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="ml-2 font-medium hover:underline dark:text-primary-500"
                 >
                   Sign in
                 </Link>
