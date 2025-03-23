249c5f5fc2ddede9b72df8e240f0e5bf482712b6
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-23 04:22:40 -0700

    [NEST-FE-48] Updated localization for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { useTranslation } from "react-i18next";
+
 export default function Footer() {
+  const { t, i18n } = useTranslation("common");
+
+  // Delay render until i18n is ready
+  if (!i18n.isInitialized) return null;
+
   return (
     <footer className="bg-tertiary text-white py-4 flex justify-between items-center px-6 z-30">
       <span className="text-xl font-semibold whitespace-nowrap text-white">
         LOGO
       </span>
       <p className="text-xs">
-        © {new Date().getFullYear()} KDG Research Center. All rights reserved.
+        © {new Date().getFullYear()} KDG Research Center.{" "}
+        {t("footer.copyright")}
       </p>
     </footer>
   );
