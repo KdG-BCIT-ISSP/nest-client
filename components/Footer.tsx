72f52ce04951cb4e9aec0d9a43ab871fe3deb5e1
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-23 04:43:19 -0700

    [NEST-FE-48] Removed localization in footer due to hydration issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,11 @@
-"use client";
-
-import { useTranslation } from "react-i18next";
-
 export default function Footer() {
-  const { t, i18n } = useTranslation("common");
-
-  // Delay render until i18n is ready
-  if (!i18n.isInitialized) return null;
-
   return (
     <footer className="bg-tertiary text-white py-4 flex justify-between items-center px-6 z-30">
       <span className="text-xl font-semibold whitespace-nowrap text-white">
         LOGO
       </span>
       <p className="text-xs">
-        © {new Date().getFullYear()} KDG Research Center.{" "}
-        {t("footer.copyright")}
+        © {new Date().getFullYear()} KDG Research Center. All rights reserved.
       </p>
     </footer>
   );
