c4c05ba698a66a4ca658135b6096f5d5308523b0
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-23 01:43:58 -0700

    [NEST-FE-48] Refactor formatdate util

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,8 +1,12 @@
 export const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
-  return date.toLocaleDateString("en-US", {
+  const d = new Date(dateString);
+  return `${d.toLocaleDateString("en-US", {
     month: "long",
     day: "numeric",
     year: "numeric",
-  });
+  })}, ${d.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  })}`;
 };
