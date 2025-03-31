bf828f224603ab9527744754b8a9a449d9b299c5
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-30 19:53:53 -0700

    [NEST-FE-Common] Cleaned reported articles

--- components/admin/ReportedArticles.tsx
+++ components/admin/ReportedArticles.tsx
@@ -5,6 +5,7 @@ import ReportCard from "@/components/admin/ReportCard";
 import { useEffect, useState } from "react";
 import { ReportPostType, Report } from "@/types/PostType";
 import { get } from "@/app/lib/fetchInterceptor";
+import { decodeAndTruncateHtml } from "@/utils/cleanHtml";
 
 export default function ReportedArticlesComponent() {
   const [loading, setLoading] = useState(true);
@@ -19,8 +20,12 @@ export default function ReportedArticlesComponent() {
           get("/api/report/article"),
           get("/api/article"),
         ]);
+        const cleanedArticles = postsData.map((post: ReportPostType) => ({
+          ...post,
+          content: decodeAndTruncateHtml(post.content),
+        }));
         setReportedPosts(reportedPostsData);
-        setPosts(postsData);
+        setPosts(cleanedArticles);
       } catch (err) {
         console.error("Failed to fetch data:", err);
       } finally {
