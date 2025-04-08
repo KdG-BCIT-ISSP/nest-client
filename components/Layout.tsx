bbb7ffdf810eecc30b69e6f20cb97ae9f8f18b4b
gdcho <gcho13@my.bcit.ca>
2025-04-07 20:39:37 -0700

    comment out notifications

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,22 +1,19 @@
 "use client";
+export const dynamic = "force-dynamic";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { userAtom } from "@/atoms/user/atom";
 
 import { useAtom } from "jotai";
 import { get } from "@/app/lib/fetchInterceptor";
 import { useTranslation } from "next-i18next";
-import { announcementAtom } from "@/atoms/announcement/atom";
+// import { announcementAtom } from "@/atoms/announcement/atom";
 
-export default function ClientLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   const { i18n } = useTranslation();
   const [, setUserData] = useAtom(userAtom);
-  const [, setAnnouncementState] = useAtom(announcementAtom); // Use announcementAtom
-  const eventSourceRef = useRef<EventSource | null>(null);
+  // const [, setAnnouncementState] = useAtom(announcementAtom); // Use announcementAtom
+  // const eventSourceRef = useRef<EventSource | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
@@ -27,31 +24,34 @@ export default function ClientLayout({
         const userData = await get("/api/member/me");
         setUserData(userData);
 
-        const eventSource = get("/api/notification/subscribe", {
-          useEventSource: true,
-        }) as EventSource;
+        // const eventSource = get("/api/notification/subscribe", {
+        //   useEventSource: true,
+        // }) as EventSource;
 
-        eventSourceRef.current = eventSource;
+        // eventSourceRef.current = eventSource;
 
-        eventSource.onopen = () => {};
+        // eventSource.onopen = () => {
+        //   console.log("SSE connection opened");
+        // };
 
-        eventSource.addEventListener(
-          "new-notification",
-          (event: MessageEvent) => {
-            const notificationData = JSON.parse(event.data);
+        // eventSource.addEventListener(
+        //   "new-notification",
+        //   (event: MessageEvent) => {
+        //     const notificationData = JSON.parse(event.data);
+        //     console.log("New notification:", notificationData);
 
-            if (notificationData.announcement) {
-              setAnnouncementState((prev) => ({
-                ...prev,
-                newAnnouncement: true,
-              }));
-            }
-          }
-        );
+        //     if (notificationData.announcement) {
+        //       setAnnouncementState((prev) => ({
+        //         ...prev,
+        //         newAnnouncement: true,
+        //       }));
+        //     }
+        //   }
+        // );
 
-        eventSource.onerror = (error) => {
-          console.error("SSE error:", error);
-        };
+        // eventSource.onerror = (error) => {
+        //   console.error("SSE error:", error);
+        // };
       } catch (error) {
         console.error("Failed to fetch profile data or subscribe:", error);
       }
@@ -59,16 +59,23 @@ export default function ClientLayout({
 
     fetchData();
 
-    return () => {
-      if (eventSourceRef.current) {
-        eventSourceRef.current.close();
-      }
-    };
-  }, [i18n.language, setUserData, setAnnouncementState]);
+    // return () => {
+    //   if (eventSourceRef.current) {
+    //     eventSourceRef.current.close();
+    //     console.log("SSE connection closed");
+    //   }
+    // };
+  }, [
+    i18n.language,
+    setUserData,
+    // setAnnouncementState,
+  ]);
 
   return (
     <div className="antialiased min-h-screen flex flex-col p-12">
-      <main className="flex-grow bg-white">{children}</main>
+      <main className="flex-grow" style={{ backgroundColor: "#ffffff" }}>
+        {children}
+      </main>
     </div>
   );
 }
